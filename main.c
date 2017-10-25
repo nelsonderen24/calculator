120ec9693d62615e0d217a386d1a6744114eb3a6
nelsonderen24 <33095123+nelsonderen24@users.noreply.github.com>
2017-10-25 16:53:52 +0200

    addition deux entiers

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int additionner(int a, int b){
+  return a+b;
+}
+
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d\n", additionner(1,4));
     return 0;
 }
