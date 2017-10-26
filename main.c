607b4521cf0caee598484ce6122149c3a5fd59c9
nelsonderen24 <33095123+nelsonderen24@users.noreply.github.com>
2017-10-26 10:31:36 +0200

    Reinitialisation calc + inclusion .H/.C

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fonctions.h"
 
-int additionner(int a, int b){
-  return a+b;
-}
-int soustraire(int a, int b){
-  return a-b;
-}
 
 int main()
 {
-    printf("a+b=%d\n", soustraire(1,4));
-    return 0;
+
 }
