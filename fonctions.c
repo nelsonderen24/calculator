22c5eb2f97b04be5c2aa3222f9f59774c5c57dd5
nelsonderen24 <33095123+nelsonderen24@users.noreply.github.com>
2017-10-26 14:26:19 +0200

    A COMPLETER

--- fonctions.c
+++ fonctions.c
@@ -1,2 +1,8 @@
 #include "fonctions.h"
 
+Fraction additionner(Fraction *fraction1, Fraction *fraction2){
+  Fraction fraction;
+  fraction.numerateur = ((fraction1->numerateur * fraction2->denominateur) + (fraction2->numerateur * fraction1->denominateur));
+  fraction.denominateur = (fraction1->denominateur * fraction2->denominateur)
+  return fraction;
+}
--- fonctions.h
+++ fonctions.h
@@ -6,20 +6,21 @@ typedef struct Fraction {
   int denominateur;
 }Fraction;
 
-Fraction ecrire();
+Fraction ecrire(); //hugo
 
-Fraction choisirOperation(Fraction *fraction1, Fraction *fraction2);
+Fraction choisirOperation(Fraction *fraction1, Fraction *fraction2); //leo
 
-void afficherResultat(Fraction fraction);
-
-
-Fraction additionner(Fraction *fraction1, Fraction *fraction2);
-Fraction diviser(Fraction *fraction1, Fraction *fraction2);
-Fraction soustraire(Fraction *fraction1, Fraction *fraction2);
-Fraction multiplier(Fraction *fraction1, Fraction *fraction2);
-Fraction comparer(Fraction *fraction1, Fraction *fraction2);
-Fraction simplifier(Fraction *fraction1);
+void afficherResultat(Fraction fraction); //benjamin
 
 
+Fraction additionner(Fraction *fraction1, Fraction *fraction2); //hugo
+Fraction diviser(Fraction *fraction1, Fraction *fraction2); //hugo
+Fraction soustraire(Fraction *fraction1, Fraction *fraction2); //leo
+Fraction multiplier(Fraction *fraction1, Fraction *fraction2);  //leo
+Fraction comparer(Fraction *fraction1, Fraction *fraction2); //benjamin
+
+Fraction simplifier(Fraction *fraction1);  //utiliser pgcd des deux nombres et diviser chaque nombre par le pgcd (nelson)
+
+int gcd(Fraction *fraction1, Fraction *fraction2); // nelson
 
 #endif // FONCTIONS_H_INCLUDED
