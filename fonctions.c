0ab1eeb24b4750fb62e59e57ef08c0b9ecbdf1d9
HugoCESI <hugo.fernandez@viacesi.fr>
2017-10-27 08:52:48 +0200

    tt

--- fonctions.c
+++ fonctions.c
@@ -2,7 +2,7 @@
 
 Fraction additionner(Fraction *fraction1, Fraction *fraction2){
   Fraction fraction;
-  fraction.numerateur = (((fraction1->numerateur) * (fraction2->denominateur)) + ((fraction2->numerateur) * (fraction1->denominateur));
+  fraction.numerateur = (((fraction1->numerateur) * (fraction2->denominateur)) + ((fraction2->numerateur) * (fraction1->denominateur)));
   fraction.denominateur = (fraction1->denominateur * fraction2->denominateur);
   return fraction;
 }
