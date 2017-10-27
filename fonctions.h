21fc803ef73b0caf13cfce121d67485a01ec5777
BenjaminDst <33086080+BenjaminDst@users.noreply.github.com>
2017-10-27 09:12:00 +0200

    ajout dans le .h

--- fonctions.h
+++ fonctions.h
@@ -7,20 +7,17 @@ typedef struct Fraction {
 }Fraction;
 
 Fraction ecrire(); //hugo
-
+void afficherFraction(Fraction *fraction);
+void comparer(Fraction fraction1, Fraction fraction2);
+int comparerbis(Fraction *fraction1, Fraction *fraction2);
 Fraction choisirOperation(Fraction *fraction1, Fraction *fraction2); //leo
-
+void metreSurMemeDenominateur(Fraction *fraction1, Fraction *fraction2);
 void afficherResultat(Fraction fraction); //benjamin
-
-
 Fraction additionner(Fraction *fraction1, Fraction *fraction2); //hugo
 Fraction diviser(Fraction *fraction1, Fraction *fraction2); //hugo
 Fraction soustraire(Fraction *fraction1, Fraction *fraction2); //leo
 Fraction multiplier(Fraction *fraction1, Fraction *fraction2);  //leo
-Fraction comparer(Fraction *fraction1, Fraction *fraction2); //benjamin
-
 Fraction simplifier(Fraction *fraction1);  //utiliser pgcd des deux nombres et diviser chaque nombre par le pgcd (nelson)
-
 int gcd(Fraction *fraction1); // nelson
 
 #endif // FONCTIONS_H_INCLUDED
