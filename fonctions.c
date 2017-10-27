35c4c97411f58b35b192e967efca614458d53fd5
HugoCESI <hugo.fernandez@viacesi.fr>
2017-10-27 09:03:53 +0200

    .c

--- fonctions.c
+++ fonctions.c
@@ -28,3 +28,28 @@ Fraction simplifier(Fraction *fraction1){
   fraction.denominateur = (fraction1->denominateur)/pgcd;
   return fraction;
 }
+
+Fraction additionner(Fraction *fraction1, Fraction *fraction2){
+  Fraction fraction;
+  fraction.numerateur = (((fraction1->numerateur) * (fraction2->denominateur)) + ((fraction2->numerateur) * (fraction1->denominateur)));
+  fraction.denominateur = (fraction1->denominateur * fraction2->denominateur);
+  return fraction;
+}
+
+Fraction diviser(Fraction *fraction1, Fraction *fraction2){
+  Fraction fraction;
+  fraction.numerateur = ((fraction1->numerateur) * (fraction1->denominateur));
+  fraction.denominateur = ((fraction2->numerateur) * (fraction2->denominateur));
+  return fraction;
+
+}
+
+Fraction ecrire(){
+  Fraction fraction;
+  printf("Entrer une fraction :\n");
+  printf("Numerateur :\n");
+  scanf("%d", &fraction.numerateur);
+  printf("Denominateur :\n");
+  scanf("%d", &fraction.denominateur);
+  return fraction;
+}
