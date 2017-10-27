bba58bfc1bd3a015761d17e1ead44a056a2460ad
nelsonderen24 <33095123+nelsonderen24@users.noreply.github.com>
2017-10-27 08:54:34 +0200

    final

--- fonctions.c
+++ fonctions.c
@@ -1,11 +1,5 @@
 #include "fonctions.h"
 
-Fraction additionner(Fraction *fraction1, Fraction *fraction2){
-  Fraction fraction;
-  fraction.numerateur = ((fraction1->numerateur * fraction2->denominateur) + (fraction2->numerateur * fraction1->denominateur));
-  fraction.denominateur = (fraction1->denominateur * fraction2->denominateur);
-  return fraction;
-}
 int gcd(Fraction *fraction1){
   int reste = 1;
   int dividende;
