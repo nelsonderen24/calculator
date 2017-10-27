6c14854ea33cc287dd443cbec1f7a4794dd69e52
BenjaminDst <33086080+BenjaminDst@users.noreply.github.com>
2017-10-27 08:53:17 +0200

    .

--- fonctions.c
+++ fonctions.c
@@ -31,7 +31,6 @@ Fraction additionner(Fraction *fraction1, Fraction *fraction2){
   fraction.numerateur = ((fraction1->numerateur * fraction2->denominateur) + (fraction2->numerateur * fraction1->denominateur));
   fraction.denominateur = (fraction1->denominateur * fraction2->denominateur); //oubli ;
   return fraction;
-
 }
 
 void comparer(Fraction fraction1, Fraction fraction2){
